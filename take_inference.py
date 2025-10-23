486bcb7dc29eef0692d7e643bd9fd502ad11d3ea
cxrbon16 <ayganyavuz@gmail.com>
2025-10-23 23:53:04 +0300

    asd

--- take_inference.py
+++ take_inference.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset, load_from_disk
-from transformers import AutoProcessor, AutoModelForVision2Seq
+from transformers import AutoProcessor, Qwen3VLForConditionalGeneration
 from PIL import Image
 import torch
 
@@ -16,19 +16,45 @@ image = example["image"]
 model_id = "Qwen/Qwen3-VL-4B-Thinking"  # veya "microsoft/phi-3-vision" da kullanılabilir
 
 processor = AutoProcessor.from_pretrained(model_id)
-model = AutoModelForVision2Seq.from_pretrained(
+model = Qwen3VLForConditionalGeneration.from_pretrained(
     model_id, torch_dtype=torch.float16, device_map="auto"
 )
+
 
 # 🔹 3. Prompt hazırla
 prompt = f"USER: I want you to solve this problem in the image. Question and answers will be both in the image. \nASSISTANT:"
 
 # 🔹 4. Görsel + text birlikte encode et
-inputs = processor(prompt, image, return_tensors="pt").to("cuda")
+
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image",
+                "image": image,
+            },
+            {"type": "text", "text": "I want you to solve this problem in the image. Question and answers will be both in the image."}
+        ],
+    }
+]
+
+inputs = processor.apply_chat_template(
+    messages,
+    tokenize=True,
+    add_generation_prompt=True,
+    return_dict=True,
+    return_tensors="pt"
+)
+inputs = inputs.to(model.device)
 
 # 🔹 5. Text generate et
-output = model.generate(**inputs, max_new_tokens=100)
-generated_text = processor.decode(output[0], skip_special_tokens=True)
 
-print("🖼️ Question:", prompt)
-print("💬 Model Output:", generated_text)
+generated_ids = model.generate(**inputs, max_new_tokens=128)
+generated_ids_trimmed = [
+    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+]
+output_text = processor.batch_decode(
+    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+)
+print(output_text)
